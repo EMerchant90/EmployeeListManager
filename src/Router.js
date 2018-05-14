e8cc31c5c8a3dfc2fe8e8d57b2ca245f50575e71
EMerchant90 <ejaz.merchant@icloud.com>
2018-05-13 23:34:33 -0400

    Created EmployeeCreate page

--- src/Router.js
+++ src/Router.js
@@ -6,12 +6,18 @@ import EmployeeList from './components/EmployeeList';
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key="root">
-        <Scene key="login" component={LoginForm} title="Please Login" initial />
-        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+      <Scene key="root" hideNavBar>
+        <Scene key="auth">
+          <Scene key="login" component={LoginForm} title="Please Login" initial />
+        </Scene>
+        <Scene key="main">
+          <Scene key="employeeList" component={EmployeeList} title="Employees" />
+        </Scene>
       </Scene>
     </Router>
   );
 };
 
 export default RouterComponent;
+
+//          //<Scene key="employeeCreate" component={EmployeeCreate} title="New Employee" />
